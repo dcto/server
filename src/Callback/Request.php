7ccd258f8176d62ce8cd3c12316273bde9baf59e
dc.To <sdoz@live.com>
2024-05-11 18:20:10 +0800

    add response to server callback

--- src/Callback/Request.php
+++ src/Callback/Request.php
@@ -14,6 +14,10 @@ class Request
      */
     protected $dispatcher;
 
+    /**
+     * @var Response
+     */
+    protected $response;
 
     /**
      * @var string
@@ -23,9 +27,13 @@ class Request
     public function __construct($container)
     {
         $this->container = $container;
+        $this->response = new Response();
     }
 
 
+    /**
+     * Handle Request Handle
+     */
     public function onRequest(\Swoole\Http\Request $request, \Swoole\Http\Response $response): void
     {
         try{
@@ -41,15 +49,19 @@ class Request
             $this->container->request->headers->add($request->header ?? []);
             $this->container->request->setMethod($request->server['request_method'] ?? 'GET');
             $this->container->request->setPathInfo($request->server['path_info'] ?? '');
-            $this->SwooleResponse($response,  $this->container->dispatch());
+
+            $psr7Response = $this->container->dispatch();
+
         }catch(\Throwable $e){
             $this->container->log->error($e);
-            if ($e->getCode() < 600) {
-                $response->status($e->getCode());
-                $response->end($e->getMessage());
-            }else{
-                $response->status(500);
-                $response->end('Internal Server Error');
+        }finally{
+            if (! isset($psr7Response)) {
+                return;
+            }
+            if (isset($psr7Request) && $psr7Request->getMethod() === 'HEAD') {
+                $this->response->emit($psr7Response, $response, false);
+            } else {
+                $this->response->emit($psr7Response, $response, true);
             }
         }
     }
@@ -66,50 +78,5 @@ class Request
     {
         $this->serverName = $serverName;
         return $this;
-    }
-    
-    /**
-    * Swoole Response
-    * @param \Swoole\Http\Response $swooleResponse
-    * @param \VM\Http\Response $response  
-    * @return void 
-    */
-    protected function SwooleResponse(\Swoole\Http\Response $swooleResponse, \VM\Http\Response $response): void
-    {
-        /*
-         * Headers
-         */
-        foreach ($response->headers() as $key => $value) {
-            $swooleResponse->header($key, implode(';', $value));
-        }
-
-        /**
-         * Cookies
-         */
-        foreach ($response->getCookies() as $cookie) {
-            /**
-             * @var \Symfony\Component\HttpFoundation\Cookie $cookie 
-             */ 
-             $swooleResponse->setcookie($cookie->getName(), $cookie->getValue(), $cookie->getExpiresTime(), $cookie->getPath(), $cookie->getDomain(), $cookie->isSecure(), $cookie->isHttpOnly(), (string) $cookie->getSameSite());
-        }
-        
-        /*
-         * Trailers
-         */
-        if (method_exists($response, 'getTrailers') && method_exists($swooleResponse, 'trailer')) {
-            foreach ($response->getTrailers() ?? [] as $key => $value) {
-                $swooleResponse->trailer($key, $value);
-            }
-        }
-
-        /**
-         * Response with swoole
-         */
-        if ($response->getResponse()){
-            $response->getContent() && $swooleResponse->write($response->getContent());
-            $swooleResponse->status($response->getStatusCode(), $response->getReasonPhrase());
-        }
-
-        $swooleResponse->end();
     }
 }
---
+++ src/Callback/Response.php
@@ -0,0 +1,97 @@
+<?php
+
+
+namespace VM\Server\Callback;
+
+
+use Psr\Http\Message\ResponseInterface;
+use Swoole\Http\Response as SwooleResponse;
+use VM\Http\Response\StreamFile;
+
+class Response {
+
+    /**
+     * @param \VM\Http\Response $response
+     * @param \Swoole\Http\Response $swooleResponse
+     * @param bool $withContent
+     */
+    public function emit(ResponseInterface $response, SwooleResponse $swooleResponse, $withContent = true)
+    {
+        try {
+            if (strtolower($swooleResponse->header['Upgrade'] ?? '') === 'websocket') {
+                return;
+            }
+            $this->swooleResponse($swooleResponse, $response);
+            $content = $response->getBody();
+            if ($content instanceof StreamFile) {
+                $swooleResponse->sendfile($content->getContents());
+                return;
+            }
+
+            if ($withContent) {
+                $swooleResponse->end((string) $content);
+            } else {
+                $swooleResponse->end();
+            }
+        } catch (\Throwable $e) {
+            \Log::debug('Swoole response emit error: '. $e->getMessage());
+        }
+    }
+
+    /**
+    * build swoole response object
+    * @version 20240511
+    */
+    protected function swooleResponse(SwooleResponse $swooleResponse, ResponseInterface $response): void
+    {
+        /*
+         * Headers
+         */
+        foreach ($response->getHeaders() as $key => $value) {
+            $swooleResponse->header($key, implode(';', $value));
+        }
+
+        /*
+         * Cookies
+         * This part maybe only supports of hyperf/http-message component.
+         */
+        if (method_exists($response, 'getCookies')) {
+            foreach ((array) $response->getCookies() as $cookie) {
+                /**
+                 * @var \Symfony\Component\HttpFoundation\Cookie $cookie 
+                 */ 
+                $swooleResponse->setcookie($cookie->getName(), $cookie->getValue(), $cookie->getExpiresTime(), $cookie->getPath(), $cookie->getDomain(), $cookie->isSecure(), $cookie->isHttpOnly(), (string) $cookie->getSameSite());
+            }
+        }
+
+        /*
+         * Trailers
+         */
+        if (method_exists($response, 'getTrailers') && method_exists($swooleResponse, 'trailer')) {
+            foreach ($response->getTrailers() ?? [] as $key => $value) {
+                $swooleResponse->trailer($key, $value);
+            }
+        }
+
+        /*
+         * Status code
+         */
+        $swooleResponse->status($response->getStatusCode(), $response->getReasonPhrase());
+    }
+
+
+    /**
+    * check if object has all methods
+    * @author  dc.To
+    * @version 20240511
+    */
+    protected function methodsExists(object $object, array $methods): bool
+    {
+        foreach ($methods as $method) {
+            if (! method_exists($object, $method)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
