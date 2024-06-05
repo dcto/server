3af7bba34e943b932f4298fb6010468999b37a07
dc.To <sdoz@live.com>
2024-06-05 14:36:37 +0800

    update Base handler

--- src/Handler/Base.php
+++ src/Handler/Base.php
@@ -17,14 +17,14 @@ class Base
         $this->container = $container;
     }
 
-    public function Connect(Server $server, int $fd)
+    public function Connect(Server $server, int $fd, int $reactorId)
     {
-        $this->container->get('log')->info('Connected: ['.$fd.']');
+        $this->container->get('log')->info(sprintf('Connect: Fd:[%s], ReactorId:[%s]', $fd, $reactorId));
     }
 
     public function Receive(Server $server, int $fd, int $reactor_id, $data)
     {
-        $this->container->get('log')->info(sprintf('Received: Fd:[%s], ReactorId:[%s], Data:%s', $fd, $reactor_id, json_encode($data)));
+        $this->container->get('log')->info(sprintf('Received: Fd:[%s], ReactorId:[%s], Data: %s', $fd, $reactor_id, $data));
     }
 
     public function Close(Server $server, int $fd,  int $reactorId)
