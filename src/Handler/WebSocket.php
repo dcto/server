2e0492cf3ecf891d1b14cad4b2c11137e2af48f3
dc.To <sdoz@live.com>
2024-06-05 10:27:51 +0800

    fixed the websocket server handler

--- src/Handler/WebSocket.php
+++ src/Handler/WebSocket.php
@@ -21,13 +21,27 @@ class WebSocket
 
     public function Open(Server $server, Request $request)
     {
+        $this->container->request->initialize(
+            $request->get ?? [],
+            $request->post ?? [],
+            [],
+            $request->cookie ?? [],
+            $request->files ?? [],
+            $request->server ?? [],
+            $request->rawContent()
+        );
+        $this->container->request->headers->add($request->header ?? []);
+        $this->container->request->setMethod($request->server['request_method'] ?? 'GET');
+        $this->container->request->setPathInfo($request->server['path_info'] ?? '');
+        
         $this->container->get('log')->info('WebSocket Connection: ['.$request->fd.']', (array) $request);
     }
 
     public function Message(Server $server, Frame $frame)
     {
-        $this->container->get('log')->info('Received Message: '.$frame->data);
-        $server->push($frame->fd, json_encode(['hello', $frame->data]));
+        $context = (array) $frame;
+        $this->container->get('log')->info('Received Message: ['.$frame->fd.']', $context);
+        $server->push($frame->fd,  $this->container->dispatch(...$context)->getContent());
     }
     
     public function HandShake(Request $request, Response $response)
